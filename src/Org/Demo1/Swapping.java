7b99631a182dc7b4cbf1a914f2d4ca4a01a68589
kirubasindhu <58856626+kirubasindhu@users.noreply.github.com>
2019-12-29 10:09:16 -0500

    after change

--- src/Org/Demo1/Swapping.java
+++ src/Org/Demo1/Swapping.java
@@ -1,7 +1,7 @@
 package Org.Demo1;
 
 public class Swapping {
-
+//after swapping
 	public static void main(String[] args) {
 		int a=20;
 		int b=30;
@@ -11,7 +11,7 @@ public class Swapping {
 		b=temp;
 		System.out.println(a);
 		System.out.println(b);
-
+System.out.println(temp);
 	}
 
 }
