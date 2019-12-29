ff5aae5939910082e0db90acbeab0f3234a0b5eb
Kalairithu02 <rk.kalai@gmail.com>
2019-12-29 20:21:24 +0530

    <First Commit> Swap --> by kalai

---
+++ src/Org/Demo1/Swapping.java
@@ -0,0 +1,17 @@
+package Org.Demo1;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
